5afd3b25afddf70bd2939a0f598d7f42670e2f4b
pricedown <josephmisaacs@gmail.com>
2025-01-28 16:14:08 -0500

    PowerShell Basic Lab - 1

--- PowerShell Basic Lab - 1/part1.ps1
+++ PowerShell Basic Lab - 1/part1.ps1
@@ -4,7 +4,6 @@ $task1 = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlia
 $task2 = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -ilike "Ethernet0" }).PrefixLength
 $task3 = Get-WmiObject -List | Where-Object { $_.Name -ilike "Win32_Net*" }
 $task4 = $task3 | Sort 
-
-(Get-CimClass -Namespace root/CIMV2 | Where-Object CimClassName -like Win32*)
-
+$task5 = Get-CimInstance Win32_NetworkAdapterConfiguration | Where-Object {$_.DHCPEnabled -eq $true} | Select-Object DHCPServer
+$task6 = $task5 | Format-Table -HideTableHeaders
 $task7 = (Get-DnsClientServerAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -ilike "Ethernet*" }).ServerAddresses[0]
--- PowerShell Basic Lab - 1/part2.ps1
+++ PowerShell Basic Lab - 1/part2.ps1
@@ -3,13 +3,13 @@
 cd $PSScriptRoot
 
 $task8 = Get-ChildItem | Where-Object { $_.Name -ilike "*ps1" }
-
  
 $folderPath = "$PSScriptRoot\outfolder"
 if (Test-Path $folderPath) {
     $task9 = Write-Host "Folder already exists"
 } else {
     $task9 = New-Item -Path $folderPath -ItemType "directory"
+    $task9
 }
 
 $task10 = Get-ChildItem | Where-Object { $_.Name -ilike "*.ps1" }
@@ -17,5 +17,5 @@ $task10 | Export-Csv -Path $folderPath\hidden-result.csv
 
 $folderPath = "$PSScriptRoot\outfolder"
 $csvFiles = Get-ChildItem -Path $folderPath | Where-Object { $_.Name -ilike "*.csv" }
-$csvFiles | Rename-Item -NewName $_.Replace(".csv", ".log")
-$task11 = Get-ChildItem | Where-Object { $_.Name -ilike "*.log" }
+$task11 = Get-ChildItem -Path $folderPath | Where-Object { $_.Name -ilike "*.log" }
+$task11
