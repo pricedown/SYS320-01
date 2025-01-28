8c89e55985e8e50cd70c13d8025757bfd25a3b63
pricedown <josephmisaacs@gmail.com>
2025-01-28 16:23:41 -0500

    Refactor first assignment

--- PowerShell Basic Lab - 1/part2.ps1
+++ PowerShell Basic Lab - 1/part2.ps1
@@ -13,9 +13,11 @@ if (Test-Path $folderPath) {
 }
 
 $task10 = Get-ChildItem | Where-Object { $_.Name -ilike "*.ps1" }
-$task10 | Export-Csv -Path $folderPath\hidden-result.csv
+$task10 | Export-Csv -Path $folderPath\out.csv
+$task10 = ls $folderPath
 
 $folderPath = "$PSScriptRoot\outfolder"
 $csvFiles = Get-ChildItem -Path $folderPath | Where-Object { $_.Name -ilike "*.csv" }
+$csvFiles | Rename-Item -NewName { $_.Name -replace ".csv", ".log" } -Force
 $task11 = Get-ChildItem -Path $folderPath | Where-Object { $_.Name -ilike "*.log" }
 $task11
