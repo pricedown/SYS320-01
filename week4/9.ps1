032145621c4214e9d650a15a670f5c040eea18db
pricedown <josephmisaacs@gmail.com>
2025-02-11 07:57:38 -0500

    Partially completed

---
+++ week4/9.ps1
@@ -0,0 +1,3 @@
+﻿. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+
+getApacheLog("i", 404, "i")
---
+++ week4/Apache-Logs.ps1
@@ -0,0 +1,35 @@
+﻿function findIPsMatchingApacheLog($pageVisited, $httpCode, $webBrowser) {
+     $matchingIPs = @()
+
+     # Get relevant logs
+     $apacheLog = "C:\xampp\apache\logs\access.log"
+     $relevantLogs = Get-Content $apacheLog | Select-String '$pageVisited', ' $httpCode ', '$webBrowser'
+
+     # https://stackoverflow.com/questions/31178400/regex-to-extract-an-ip-address-from-a-string
+     $regex = [regex] "\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
+     $relevantIPs = $regex.Matches($relevantLogs)
+
+     # Loop to extract ips
+     for ($i = 0; $i -lt $relevantIPs.Count; $i++) {
+        $matchingIPs += [pscustomobject]@{ "IP" = $relevantIPs[$i].Value }
+     }
+     
+     $uniqueIPs = $matchingIPs
+     return $uniqueIPs
+}
+
+$log =  findIPsMatchingApacheLog(' ', '404', ' ')
+$log
+
+    
+
+     # Get relevant logs
+     $apacheLog = "C:\xampp\apache\logs\access.log"
+     $relevantLogs = Get-Content $apacheLog | Select-String ' ', ' 404 ', ' '
+     $relevantLogs
+          $regex = [regex] "\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
+     $relevantIPs = $regex.Matches($relevantLogs)
+     $relevantIPs
+
+$log =  findIPsMatchingApacheLog(' ', '404', ' ')
+$log
