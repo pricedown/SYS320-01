102ea99c43226b875128c3166a63e5679c4be0b0
pricedown <josephmisaacs@gmail.com>
2025-02-19 11:48:12 -0500

    Almost done

---
+++ week5/ScrapingChamplainClasses.ps1
@@ -0,0 +1,45 @@
+﻿clear
+
+function gatherClasses() {
+    $page = Invoke-WebRequest -TimeoutSec 2 http://localhost/Courses.html
+    $trs = $page.ParsedHTML.body.getElementsByTagName("tr")
+
+    $FullTable = @()
+    for ($i=1; $i -lt $trs.length; $i++) {
+        $tds = $trs[$i].getElementsByTagName("td")
+        $Times = $tds[5].innerText.Split("-")
+
+      
+        #for ($n=0; $n -lt $tds.length; $n++) { "$n : $($tds[$n].innerText | Out-String)" | Out-String }
+        
+        
+        $FullTable += [pscustomobject]@{ "ClassCode" = $tds[0].innerText; `
+                                         "Title" = $tds[1].innerText; `
+                                         "Days" = $tds[4].innerText; `
+                                         "Time Start" = $Times[0]; `
+                                         "Time End" = $Times[1]; `
+                                         "Instructor" = $tds[6].innerText; `
+                                         "Location" = $tds[9].innerText; `
+        } #>
+    }
+    return $FullTable
+}
+
+function daysTranslator($FullTable) {
+    for ($i=1; $i -lt $FullTable.length; $i++) {
+        $Days = @()
+
+        $old = $FullTable[$i].Days;
+
+        if ($old -ilike "*M*") { $Days += "Monday" }
+
+        if ($old -ilike "*T[TWF]*") { $Days += "Tuesday" }
+        elseif ($old -ilike "T") { $Days += "Tuesday" }
+
+        if ($old -ilike "*W*") { $Days += "Wenesday" }
+        if ($old -ilike "*TH*") { $Days += "Thursday" }
+        if ($old -ilike "*F*") { $Days += "Friday" }
+
+        $FullTable[$i].Days = $Days 
+    }
+}
---
+++ week5/scraping-main.ps1
@@ -0,0 +1,32 @@
+﻿. (Join-Path $PSScriptRoot "ScrapingChamplainClasses.ps1")
+
+$FullTable = gatherClasses
+daysTranslator($FullTable)
+
+
+# List all the classes of the instructor furkan paligu
+$i = $FullTable | 
+
+# List all the classes of JOYC 310 on Mondays, only display Class Code and Times. Sort by Start Time
+$ii = $FullTable | Where-Object { ($_.Location -ilike "JOYC 310") -and ($_.days -contains "Monday" ) }
+$ii = _______ "Time Start" | `
+      _______ "Time Start", "Time End", "Class Code"
+$ii
+
+# Make a list for all the instructors that teach at least 1 course in one of the courses:
+# SYS, NET, SEC, FOR, CSI, DAT. Sort by name and make it unique
+$ITSInstructors = $FullTable | Where-Object { ($_."Class Code" -ilike "SYS*") -or `
+                                              ($_."Class Code" -ilike "NET*") -or `
+                                              ($_."Class Code" -ilike "SEC*") -or `
+                                              ($_."Class Code" -ilike "FOR*") -or `
+                                              ($_."Class Code" -ilike "CSI*") -or `
+                                              ($_."Class Code" -ilike "DAT*") -or }
+                             | _____ "Instructor" `
+                             | _____ "Instructor" -____
+
+$ITSInstructors
+
+$iv = FullTable | ____ { $_.Instructor -in $ITSInstructors.Instructor }
+                 | ________ "Instructor" | ______ Count,Name | ______ Count -Descending
+
+
