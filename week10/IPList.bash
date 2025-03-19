4d65bedee2494115bf6b07b68578b649ba5a6df6
pricedown <josephmisaacs@gmail.com>
2025-03-19 13:12:12 -0400

    IPList first deliverable

---
+++ week10/IPList.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+# List all of the ips in the given network prefix
+
+
+[ $# -lt 1 ] && echo "Usage: $0 <Prefix>" && exit 1
+prefix=$1
+
+[ ${#prefix} -lt 5 ] && \
+	printf "Prefix length is too short\nExample: 10.0.17\n" && \
+	exit 1
+
+for i in {1..255} 
+do
+	ip="$prefix.$i"
+	ping -c 1 ip
+done
