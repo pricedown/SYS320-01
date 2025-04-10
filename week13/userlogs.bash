a8ad3181427ffb735c68e9ea434e318e8b83086a
pricedown <josephmisaacs@gmail.com>
2025-04-10 11:15:01 -0400

    Week13 init

---
+++ week13/userlogs.bash
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+authfile="/var/log/auth.log"
+
+function getLogins() {
+    logline=$(cat "$authfile" | grep "systemd-logind" | grep "New session")
+    dateAndUser=$(echo "$logline" | cut -d' ' -f1,2,11 | tr -d '\.')
+    echo "$dateAndUser"
+}
+
+#function getFailedLogins(){
+# Todo - 1
+# a) Make a little research and experimentation to complete the function
+# b) Generate failed logins and test
+#}
+
+# Sending logins as email - Do not forget to change email address
+# to your own email address
+echo "To: joseph.isaacs@champlain.edu" >emailform.txt
+echo "Subject: Logins" >>emailform.txt
+getLogins >>emailform.txt
+cat emailform.txt | msmtp fpaligu@champlain.edu # using msmtp because Google won't auth with ssmtp
+
+# Todo - 2
+# Send failed logins as email to yourself.
+# Similar to sending logins as email
