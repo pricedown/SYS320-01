1f7f97d18b1214650d69627a85c610c0eb0fb5d0
pricedown <josephmisaacs@gmail.com>
2025-04-12 09:28:32 -0400

    Fixing install lvim

--- utils/install-lvim.bash
+++ utils/install-lvim.bash
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo snap install nvim --classic
-sudo apt-get install gcc
+# sudo apt-get update
+# sudo snap install nvim --classic
+# sudo apt-get install gcc
+#
+# # backups
+# mv ~/.config/nvim{,.bak}
+# mv ~/.local/share/nvim{,.bak}
+# mv ~/.local/state/nvim{,.bak}
+# mv ~/.cache/nvim{,.bak}
+#
+# # LazyVim
+# git clone https://github.com/LazyVim/starter ~/.config/nvim
+# rm -rf ~/.config/nvim/.git
+#
+# # More configuration
+#
+# nvim
 
-# backups
-mv ~/.config/nvim{,.bak}
-mv ~/.local/share/nvim{,.bak}
-mv ~/.local/state/nvim{,.bak}
-mv ~/.cache/nvim{,.bak}
+cd "$(dirname "$0")"
+scriptloc="$(pwd)"
 
-# LazyVim
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
-
-# More configuration
-
-nvim
-
-cp -rf ./lvim-cfg/options.lua ~/.config/nvim/lua/config/options.lua
-cp -rf ./lvim-cfg/colorscheme.lua ~/.config/nvim/lua/plugins/colorscheme.lua
+cp -rf $scriptloc/lvim-cfg/options.lua ~/.config/nvim/lua/config/options.lua
+cp -rf $scriptloc/lvim-cfg/colorscheme.lua ~/.config/nvim/lua/plugins/colorscheme.lua
