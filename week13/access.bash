557888ee55de8848362856cdc1f03bf327819e78
pricedown <josephmisaacs@gmail.com>
2025-04-16 08:02:39 -0400

    Debugging

--- week13/access.bash
+++ week13/access.bash
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+set -e
+export HOME=/home/champuser
+export PATH=/usr/loca/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export LOGNAME=champuser
 
 accesslog="/home/champuser/SYS320-01/week13/fileaccesslog.txt"
 access_email="/home/champuser/SYS320-01/week13/access_email.txt"
@@ -8,6 +12,7 @@ echo "$access_line" >>"$accesslog"
 
 echo "To: joseph.isaacs@mymail.champlain.edu" >"$access_email"
 echo "Subject: Access" >>"$access_email"
-echo "$access_line" >>"$access_email"
+echo "Hello, here are the accesses" >>"$access_email"
+cat "$accesslog" >>"$access_email"
 #tail $accesslog -n 1 >>"$access_email"
-cat $access_email | /bin/msmtp joseph.isaacs@mymail.champlain.edu
+cat $access_email | /usr/bin/msmtp joseph.isaacs@mymail.champlain.edu >>/tmp/access_debug.log
