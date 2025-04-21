100ab960f4d87c8276c917ef458954d6fb76210d
pricedown <josephmisaacs@gmail.com>
2025-04-21 13:35:55 -0400

    readingtables attempt

--- week14/readingtables.bash
+++ week14/readingtables.bash
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
 page=http://10.0.17.6/Assignment.html
+html=$(curl -sL $page)
+
+cleaned=$(echo "$html" | sed 's/<[^>]*>//g' | sed '/^[[:space:]]*$/d')
+
+temp_vals=$(echo "$cleaned" | sed -n '/The Temprature Read:/, /The Pressure Read:/p' | sed -n '/[0-9]\{2\}/p')
+press_vals=$(echo "$cleaned" | sed -n '/The Pressure Read:/,$p' | sed -n '/[0-9]\{2\}/p')
+
+count=$(echo "$temp_vals" | wc -l)
+
+for ((i = 1; i <= count; i += 2)); do
+    temp=$(echo "$temp_vals" | head -n $i | tail -n 1)
+    press=$(echo "$press_vals" | head -n $i | tail -n 1)
+    date=$(echo "$temp_vals" | head -n $(($i + 1)) | tail -n 1)
+
+    echo "$press"
+    echo "$temp"
+    echo "$date"
+done
