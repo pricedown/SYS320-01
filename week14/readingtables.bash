416b8da8b08395bba7900e0eb417a702138e5d98
pricedown <josephmisaacs@gmail.com>
2025-04-22 19:09:23 -0400

    Finally debugged carriage returns

--- week14/readingtables.bash
+++ week14/readingtables.bash
@@ -3,7 +3,7 @@
 page=http://10.0.17.6/Assignment.html
 html=$(curl -sL $page)
 
-cleaned=$(echo "$html" | sed 's/<[^>]*>//g' | sed '/^[[:space:]]*$/d')
+cleaned=$(echo "$html" | sed 's/<[^>]*>//g' | sed '/^[[:space:]]*$/d' | tr -d '\t' | tr -d '\r')
 
 temp_vals=$(echo "$cleaned" | sed -n '/The Temprature Read:/, /The Pressure Read:/p' | sed -n '/[0-9]\{2\}/p')
 press_vals=$(echo "$cleaned" | sed -n '/The Pressure Read:/,$p' | sed -n '/[0-9]\{2\}/p')
@@ -15,7 +15,5 @@ for ((i = 1; i <= count; i += 2)); do
     press=$(echo "$press_vals" | head -n $i | tail -n 1)
     date=$(echo "$temp_vals" | head -n $(($i + 1)) | tail -n 1)
 
-    echo "$press"
-    echo "$temp"
-    echo "$date"
+    echo "$press $temp $date"
 done
