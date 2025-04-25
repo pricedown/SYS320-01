56914d9b2dd2f9296effa130978f73315f089aa3
pricedown <josephmisaacs@gmail.com>
2025-04-25 08:10:44 -0400

    networkchecker completed

--- week15/networkchecker.bash
+++ week15/networkchecker.bash
@@ -4,8 +4,8 @@ myIP=$(bash myIP.bash)
 
 # Todo-1: Create a helpmenu function that prints help for the script
 function helpmenu() {
-    echo "                    HELP MENU                     "
-    echo "--------------------------------------------------"
+    echo "                       HELP MENU                       "
+    echo "-------------------------------------------------------"
     echo "-n: Add -n as an argument for this script to use nmap"
     echo " -n external: External NMAP scan"
     echo " -n internal: Internal NMAP scan"
@@ -13,8 +13,8 @@ function helpmenu() {
     echo " -s external: External ss(Netstat) scan"
     echo " -s internal: Internal ss(Netstat) scan"
     echo ""
-    echo "Usage: bash networkchecker.bash -n/--s external/internal"
-    echo "--------------------------------------------------"
+    echo "Usage: bash networkchecker.bash -n/-s external/internal"
+    echo "-------------------------------------------------------"
 }
 
 # Return ports that are serving to the network
@@ -31,7 +31,12 @@ function InternalNmap() {
 function ExternalListeningPorts() {
     # Todo-2: Complete the ExternalListeningPorts that will print the port and application
     # that is listening on that port from network (using ss utility)
-    elpo=$(ss -tulnH | grep -v 127.0.0.1)
+    elpo=$(sudo ss -pltnH | grep -v 127.0.0.1 | awk '{print $5, $6}' | while read -r addr users; do
+        port=$(echo "$addr" | awk -F':' '{print $NF}')
+        application=$(echo "$users" | cut -d'"' -f2)
+        echo "$port $application"
+    done)
+
 }
 
 # Only IPv4 ports listening from localhost
@@ -40,6 +45,10 @@ function InternalListeningPorts() {
 }
 
 # Todo-3: If the program is not taking exactly 2 arguments, print helpmenu
+if [ "$#" -ne 2 ]; then
+    helpmenu
+    exit 1
+fi
 
 # Todo-4: Use getopts to accept options -n and -s (both will have an argument)
 # If the argument is not internal or external, call helpmenu
@@ -48,20 +57,14 @@ function InternalListeningPorts() {
 # For instance: -n internal => will call NMAP on localhost
 #               -s external => will call ss on network (non-local)
 
-if [ "$#" -ne 2 ]; then
-    helpmenu
-    echo "You only had $# arguments"
-    exit 1
-fi
-
-argument=$2
+internalOrExternal=$2
 while getopts "ns" option; do
     case "$option" in
     n)
-        if [[ "$argument" == "internal" ]]; then
+        if [[ "$internalOrExternal" == "internal" ]]; then
             InternalNmap
             echo "$rin"
-        elif [[ "$argument" == "external" ]]; then
+        elif [[ "$internalOrExternal" == "external" ]]; then
             ExternalNmap
             echo "$rex"
         else
@@ -70,10 +73,10 @@ while getopts "ns" option; do
         fi
         ;;
     s)
-        if [[ "$argument" == "internal" ]]; then
+        if [[ "$internalOrExternal" == "internal" ]]; then
             InternalListeningPorts
             echo "$iplo"
-        elif [[ "$argument" == "external" ]]; then
+        elif [[ "$internalOrExternal" == "external" ]]; then
             ExternalListeningPorts
             echo "$elpo"
         else
